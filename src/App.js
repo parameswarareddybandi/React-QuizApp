44958b3fbf1ffe465f0ef91c566281fc5097b6a0
pardhasaradhi82 <20r01a05a1@cmritonline.ac.in>
2023-08-03 04:56:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { questions } from "./Questions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentQsnIndex, updateIndex] = useState(0);
+  const currentQsn = questions[currentQsnIndex];
+  const onFirstQuestion = currentQsnIndex === 0;
+  const onLastQuestion = currentQsnIndex === questions.length - 1;
+
+  function validateOption(e) {
+    var ans = +e.target.value;
+
+    if (ans === questions[currentQsnIndex].correctAnswer) {
+      document.body.style.background = 'green';
+      const radioButtons = document.getElementsByName("options");
+      for (let i = 0; i < radioButtons.length; i++) {
+        radioButtons[i].disabled = true;
+      }
+    } else {
+      document.body.style.background = 'red';
+      const radioButtons = document.getElementsByName("options");
+      for (let i = 0; i < radioButtons.length; i++) {
+        radioButtons[i].disabled = true;
+      }
+    }
+
+    setTimeout(() => {
+      document.body.style.background = 'rgb(69, 68, 68)';
+    }, 3000);
+  }
+
+  function deselectOption() {
+    const radioButtons = document.getElementsByName("options");
+
+    for (let i = 0; i < radioButtons.length; i++) {
+      radioButtons[i].checked = false;
+    }
+  }
+
+
+  if (currentQsnIndex < questions.length) {
+    return (
+      <div>
+        <h1> React Quiz </h1>
+        <img src="https://th.bing.com/th/id/OIP.O5TtputgI0PbmDwQ-9Nu9wHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt='quizApp-logo' />
+        <div id="questionForm">
+          <h3>{currentQsn.question}</h3></div>
+          <div id="optionForm">
+          <ul>
+            {currentQsn.answers.map((a, i) => (
+              <li key={"qsn_" + i}><input type="radio" name="options" value={i} onClick={(e) => { validateOption(e) }} required />{a}</li>
+            ))}
+          </ul>
+        </div>
+        <div id="buttons">
+          <button onClick={() => {
+            updateIndex(prevIndex => prevIndex - 1);
+            const radioButtons = document.getElementsByName("options");
+            for (let i = 0; i < radioButtons.length; i++) {
+              radioButtons[i].disabled = false;
+            }
+          }} disabled={onFirstQuestion}>Prev</button>
+          <button onClick={() => {
+            updateIndex(prevIndex => prevIndex + 1);
+            deselectOption();
+            const radioButtons = document.getElementsByName("options");
+            for (let i = 0; i < radioButtons.length; i++) {
+              radioButtons[i].disabled = false;
+            }
+          }} disabled={onLastQuestion}>Next</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Thank You</h1>
+        <button onClick={() => { updateIndex(0); }}>Reset</button>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
---
+++ src/Questions.js
@@ -0,0 +1,63 @@
+export const questions = [
+    {
+        "id": 1,
+        "question": "Which of the following command is used to install create-react-app?",
+        "answers": ["npm install -g create-react-app", "npm install create-react-app", "npm install -f create-react-app", "install -g create-react-app"],
+        "correctAnswer": 0,
+    },
+    {
+        "id": 2,
+        "question": "What of the following is used in React.js to increase performance?",
+        "answers": ["Original DOM", "Virtual DOM", "Both A and B.", "None of the above."],
+        "correctAnswer": 1,
+    },
+    {
+        "id": 3,
+        "question": "Which of the following acts as the input of a class-based component?",
+        "answers": ["Class", "Factory", "Render", "Props"],
+        "correctAnswer": 3,
+    },
+    {
+        "id": 4,
+        "question": "How many ways of defining your variables in ES6?",
+        "answers": ["1", "3", "4", "5"],
+        "correctAnswer": 1,
+    },
+    {
+        "id": 5,
+        "question": "What is a state in React?",
+        "answers": ["A permanent storage.", "Internal storage of the component.", "External storage of the component.", "None of the above."],
+        "correctAnswer": 1,
+    },
+    {
+        "id": 6,
+        "question": "What are the two ways to handle data in React?",
+        "answers": ["State & Props", "Services & Components", "State & Services", "State & Component"],
+        "correctAnswer": 0,
+    },
+    {
+        "id": 7,
+        "question": "Which of the following is a must API for every React.js component?",
+        "answers": ["SetinitialComponent", "renderComponent", "render", "All of the above"],
+        "correctAnswer": 1,
+    },
+    {
+        "id": 8,
+        "question": "Which of the following is used to pass data to a component from outside in React.js?",
+        "answers": ["SetState", "Render with arguments", "Props", "PropTypes"],
+        "correctAnswer": 2,
+    },
+    {
+        "id": 9,
+        "question": "What is true for the keys given to a list of elements in React?",
+        "answers": ["Unique in the DOM.", "Unique among the siblings only.", "Do not require to be unique.", "None of the above."],
+        "correctAnswer": 1,
+    },
+    {
+        "id": 10,
+        "question": "Which of the following are the advantages of React.js?",
+        "answers": ["React.js can increase the application's performance with Virtual DOM.", "React.js is easy to integrate with other frameworks such as Angular, BackboneJS since it is only a view library.", "React.js can render both on client and server side.", "All of the above"],
+        "correctAnswer": 3,
+    }
+
+]
